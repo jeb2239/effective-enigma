9e324494fd39fd71f65e31bef1988e7e98345ba3
Jonathan Barrios <jeb2239@columbia.edu>
2017-06-02 18:55:25 -0400

    project set up

--- server/boot/authentication.ts
+++ server/boot/authentication.ts
@@ -3,4 +3,5 @@
 module.exports = function enableAuthentication(server) {
   // enable authentication
   server.enableAuth();
+
 };
--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,21 @@
-'use strict';
 
+
+// import 'loopback' as loopback
+// import 'loopback-boot' as boot
 var loopback = require('loopback');
 var boot = require('loopback-boot');
 
-var app = module.exports = loopback();
+export const app  = loopback();
 
 app.start = function() {
   // start the web server
+
   return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
+
       var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
@@ -23,7 +27,7 @@ app.start = function() {
 boot(app, __dirname, function(err) {
   if (err) throw err;
 
+  
   // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+  if (require.main === module) app.start();
 });
