ad69081e462e80543e97b5b6fef5b67639e43730
shubhamonweb <shubhamonweb@gmail.com>
2017-06-04 09:02:12 -0400

    h

--- client/elm/watson_bindings.js
+++ client/elm/watson_bindings.js
@@ -53,60 +53,30 @@ function Hello(){
 //      }
 // });
     console.log(getWatson("where am I?"));
-    // conversation.message({}, processResponse);
 }
-
-function getWatson(str) {
-  // try{
-  // conversation.message({
-  //     input: { text: str },
-  //     // Send back the context to maintain state.
-  //     context : response.context,
-  //   }, processResponse)
-  //   return response.output.text[0];
-  // } catch(err) {
-  //       console.log(err);
-  //   }
-  console.log("gfds")
-  return conversation.message({}, processResponse);
-}
-function processResponse(err, response) {
-  if (err) {
-    console.error(err); // something went wrong
-    return;
-  }
-  return response.output.text[0]
-  // var endConversation = false;
-  
-  // Check for action flags.
-  // if (response.output.action === 'display_time') {
-  //   // User asked what time it is, so we output the local system time.
-  //   console.log('The current time is ' + new Date().toLocaleTimeString());
-  // } else if (response.output.action === 'end_conversation') {
-  //   // User said goodbye, so we're done.
-  //   console.log(response.output.text[0]);
-  //   endConversation = true;
-  // } else {
-  //   // Display the output from dialog, if any.
-  //   if (response.output.text.length != 0) {
-  //       console.log(response.output.text[0]);
-  //   }
-  // }
-
-  // If we're not done, prompt for the next round of input.
-  // if (!endConversation) {
-  //   var newMessageFromUser = prompt('>> ');
-  //   conversation.message({
-  //     input: { text: newMessageFromUser },
-  //     // Send back the context to maintain state.
-  //     context : response.context,
-  //   }, processResponse)
-  // }
-}
-
 module.exports = {
-  run: function () {
-    console.log('run from library');
-    Hello();
-  }
-};
+ getWatson :function(str) {
+
+  conversation.message({
+  input: { text: str }//,
+  // workspace_id: '<workspace id>'
+  }, function(err, response) {
+       if (err) {
+         console.error(err);
+       } else {
+         console.log(JSON.stringify(response.output.text[0], null, 2));
+         var ret = JSON.stringify(response.output.text[0], null, 2); //response.output.text[0];
+         // console.log(ret)
+         return ret;
+       }
+  });
+  // return "hi..." ;
+}
+}
+
+// module.exports = {
+//   run: function () {
+//     console.log('run from library');
+//     Hello();
+//   }
+// };
